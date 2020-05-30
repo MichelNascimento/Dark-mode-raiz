a30aa88e4e76e0555dc30dfbe2707f395a116956
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-05-29 23:20:17 -0300

    Criação da função para realizar a mudança de cores

--- scripts.js
+++ scripts.js
@@ -1,20 +1,20 @@
 const html = document.querySelector("html");
 const checkbox = document.querySelector("input[name=theme]");
 
-// função para pegar o Computed Style do documento.
+// função para pegar o Computed Style do documento (HTML).
 const getStyles = (element, style) =>
     window.getComputedStyle(element).getPropertyValue(style);
 
-// cores inicias do layout (light).
+// criando um objeto com as cores iniciais do layout (light).
 const initialColors = {
-    // aqui usamos a função criada acima, passando o elemento e a propriedade exata que vamos usar como parâmetros.
+    // aqui usamos a função criada acima, passando como parâmetros o elemento e as propriedades de estilos exatas que vamos usar.
     bg: getStyles(html, "--bg"),
     bgPanel: getStyles(html, "--bg-panel"),
     colorHeadings: getStyles(html, "--color-headings"),
     colorText: getStyles(html, "--color-text"),
 }
 
-// cores do modo noturno.
+// criando um objeto com as cores do modo noturno.
 const darkMode = {
     bg: "#333333",
     bgPanel: "#434343",
@@ -22,11 +22,19 @@ const darkMode = {
     colorText: "#B5B5B5",
 }
 
-const changeColors = (colors) => {
+// função para setar o nome da chave do objeto de cores, igual ao nome da propriedade.
+// nesse RegEx, estamos buscando todas as letras maiusculas, e colocando um traço antes delas. Por último, formatando para letra minuscula.
+const transformKey = key => 
+    "--" + key.replace(/([A-Z])/, "-$1").toLowerCase()
 
+const changeColors = (colors) => {
+    Object.keys(colors).map(key => {
+        html.style.setProperty(transformKey(key), colors[key])
+    })
 }
 
-
+// ouvindo o evento de mudança (change) do checkbox, para validar se função de alteração de cores (changeColors) 
+// receberá as propriedade do modo light ou dark.
 checkbox.addEventListener("change", ({ target }) => {
-    target.checked ? changeColors() : changeColors()
+    target.checked ? changeColors(darkMode) : changeColors(initialColors)
 })
